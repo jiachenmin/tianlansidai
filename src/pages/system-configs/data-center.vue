2c307ebbff0ce9b4eeec9905ac62b60d0de8c134
贾辰敏 <1620415672@qq.com>
2020-03-15 21:21:17 +0800

    第五次提交

--- src/pages/system-configs/data-center.vue
+++ src/pages/system-configs/data-center.vue
@@ -10,20 +10,20 @@
           </div>
           <Divider />
 
-          <ul class="date-list">
-            <li v-for="(item,index) in data1" :key="item._id" @click="getkey($event,item._id)">
-              <router-link to>
-                <Button type="text" ghost>{{item.name}}</Button>
-              </router-link>
-            </li>
-          </ul>
+         <Tree :data="resourceTree" @on-select-change="getAtherData"></Tree>
         </Card>
       </i-col>
       <i-col span="18" class-name="right-box">
         <Card>
-          <Table height="800" :columns="columns1" :data="data2">
-            <Button type="error">Error</Button>
+               <div class="title">
+            <i-col span="10">
+              <h3>{{name}}</h3>
+            </i-col>
+          </div>
+          <Divider />
+          <Table :columns="columns1" border :data="resourceDate">
           </Table>
+          <Page :current="pageInfo.page" :pageSize="pageInfo.size" :total="pageInfo.total" @on-change="onPageChange" on-page-size-change="onPageSizeChange" show-sizer show-total/>
         </Card>
       </i-col>
     </Row>
@@ -31,86 +31,153 @@
 </template>
 <script>
 import {
-  getDataCenter,
-  getResourceList
-} from "@/api/system-configs/data-center.js";
+  getDataCenter, getResourceList
+} from '@/api/system-configs/data-center.js'
 
 export default {
-  created() {
-    this.DogetDataCenter();
+  created () {
+    this.doGetResourceDate()
   },
   // updated(){
   //   this.getkey();
   // },
-  data() {
+  data () {
     return {
-      // name:'',
-      data1: [],
+      name: '',
+      resourceTree: [],
       columns1: [
         {
-          title: "#",
-          key: ""
+          title: '#',
+          type: 'index'
         },
         {
-          title: "编号",
-          key: "number"
+          title: '编号',
+          key: 'number'
         },
         {
-          title: "名称",
-          key: "name"
+          title: '名称',
+          key: 'name'
         },
         {
-          title: "备注",
-          key: "memo"
+          title: '备注',
+          key: 'memo'
         },
         {
-          title: "操作",
-          key: "caozuo"
+          title: '操作',
+          key: 'caozuo'
         }
       ],
-      data2: [],
-      typeId:'5c9c3339ab84d455fc483b82'
-    };
+      pageInfo: {
+        page: 1,
+        size: 10,
+        total: 0
+      },
+      resourceDate: [],
+      curryTypeId: ''
+    //   typeId:'5c9c3339ab84d455fc483b82'
+    }
   },
   methods: {
-    DogetDataCenter() {
-      getDataCenter().then(res => {
-        if (res.success) {
-          var result = res.data;
-          console.log(result);
-          this.data1 = result.data;
-          var params = {
-            typeId: this.typeId,
-            // keyWord: '',
-            pageNum: 1,
-            pageSize: 10
-          };
-          console.log(params.typeId);
-          getResourceList(params).then(res => {
-            if (res.success) {
-              console.log(res);
-              var result = res.data;
-              console.log(result);
-              this.data2 = result.data.list;
-            }
-          });
+    async doGetResourceDate () {
+      const result = await getDataCenter()
+      const { data } = result.data
+      console.log(data)
+      this.resourceTree = data.map((elm, index) => {
+        const params = {
+          id: elm._id,
+          title: elm.name,
+          selected: false
         }
-      });
-    },
-    getkey(e,k){
-// console.log(k)
-var params={
-  typeId:k,
-  pageNum:1,
-  pageSize:10
+        console.log(elm)
+        if (!index) {
+          params.selected = true
+        //   this.doGetResource({
+        //     pageNum: this.pageInfo.page,
+        //     pageSize: this.pageInfo.size,
+        //     typeId: params.id
+
+        //   })
+        this.getAtherData(null, params)
+this.name='' //这有问题
+        this.name = params.title
+
 }
-this.typeId='';
-this.typeId=params.typeId;
-console.log(params.typeId)
-this.DogetDataCenter();
-    }
+        return params
+
+      })
+    },
+    getAtherData (selArr, selNode) {
+      this.curryTypeId = selNode.id
+      this.doGetResource({
+        pageNum: this.pageInfo.page,
+        pageSize: this.pageInfo.size,
+        typeId: this.curryTypeId
+})
+    },
+    onPageChange (page) {
+      this.pageInfo.page = page
+      console.log(page)
+      debugger
+      this.doGetResource({
+        pageNum: page,
+        pageSize: this.pageInfo.size,
+        typeId: this.curryTypeId
+})
+    },
+    onPageSizeChange (size) {
+      this.pageInfo.size = size
+      console.log(size)
+      this.doGetResource({
+        pageNum: this.pageInfo.page,
+        pageSize: size,
+        typeId: this.curryTypeId
+      })
+    },
+    async doGetResource (params) {
+      const result = await getResourceList(params)
+      const { data } = result.data
+      this.resourceDate = data.list
+      this.pageInfo.total = data.total
+
+},
+// DogetDataCenter() {
+//       getDataCenter().then(res => {
+//         if (res.success) {
+//           var result = res.data;
+//           console.log(result);
+//           this.data1 = result.data;
+//           var params = {
+//             typeId: this.typeId,
+//             // keyWord: '',
+//             pageNum: 1,
+//             pageSize: 10
+//           };
+//           console.log(params.typeId);
+//           getResourceList(params).then(res => {
+//             if (res.success) {
+//               console.log(res);
+//               var result = res.data;
+//               console.log(result);
+//               this.data2 = result.data.list;
+//             }
+//           });
+//         }
+//       });
+//     },
+//     getkey(e,k){
+// // console.log(k)
+// var params={
+//   typeId:k,
+//   pageNum:1,
+//   pageSize:10
+// }
+// this.typeId='';
+// this.typeId=params.typeId;
+// console.log(params.typeId)
+// this.DogetDataCenter();
+//     }
   }
-};
+}
 </script>
 <style lang="less" scoped>
 .left-box,
